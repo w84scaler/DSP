807a9534ee4eb2179839c42395df80495888ca06
w84scaler <saint.neki@gmail.com>
2021-09-23 01:50:37 +0300

    1st lab done

---
+++ DSP1/DSP1/ApplicationViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DSP1
+{
+    class ApplicationViewModel : INotifyPropertyChanged
+    {
+        private List<Signal> selectedSignals;
+        public List<Signal> SelectedSignals
+        {
+            get { return selectedSignals; }
+            set
+            {
+                selectedSignals = value;
+                OnPropertyChanged("SelectedSignals");
+            }
+        }
+
+        private RelayCommand pageCommand;
+        public RelayCommand PageCommand
+        {
+            get
+            {
+                return pageCommand ??
+                    (pageCommand = new RelayCommand(num =>
+                    {
+                        SelectedSignals = SignalLists[Int32.Parse((string)num)];
+                    }));
+            }
+        }
+
+        public ObservableCollection<List<Signal>> SignalLists { get; set; }
+        public ApplicationViewModel()
+        {
+            SignalLists = new ObservableCollection<List<Signal>>
+            {
+                new List<Signal>
+                {
+                    new Signal(1024, 6, 3, Math.PI * 2),
+                    new Signal(1024, 6, 3, Math.PI / 6),
+                    new Signal(1024, 6, 3, Math.PI / 2),
+                    new Signal(1024, 6, 3, 0),
+                    new Signal(1024, 6, 3, Math.PI * 3 / 4)
+                },
+                new List<Signal>()
+                {
+                    new Signal(1024, 8, 2, Math.PI / 4),
+                    new Signal(1024, 8, 4, Math.PI / 4),
+                    new Signal(1024, 8, 3, Math.PI / 4),
+                    new Signal(1024, 8, 7, Math.PI / 4),
+                    new Signal(1024, 8, 5, Math.PI / 4)
+                },
+                new List<Signal>()
+                {
+                    new Signal(1024, 2, 5, Math.PI / 4),
+                    new Signal(1024, 2, 5, Math.PI / 4),
+                    new Signal(1024, 8, 5, Math.PI / 4),
+                    new Signal(1024, 3, 5, Math.PI / 4),
+                    new Signal(1024, 1, 5, Math.PI / 4)
+                },
+                new List<Signal>()
+                {
+                    new Signal(1024, 2, new double[] { 1, 2, 3, 4, 5 }, new double[] { Math.PI / 6, Math.PI / 2, Math.PI / 3, Math.PI / 9, 0 })
+                },
+                new List<Signal>()
+                {
+                    new Signal(1024, 2, 8, Math.PI / 3, 0.2, false)
+                },
+            };
+            SelectedSignals = SignalLists[0];
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ DSP1/DSP1/Signal.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+using OxyPlot;
+
+namespace DSP1
+{
+    class Signal
+    {
+        public List<DataPoint> Points { get; set; }
+
+        public Signal(int N, double A, double f, double fi)
+        {
+            Points = new List<DataPoint>();
+            for (int n = 0; n < N; n++)
+            {
+                Points.Add(new DataPoint(n, A * Math.Sin(2 * Math.PI * f * n / N + fi)));
+            }
+        }
+
+        public Signal(int N, double A, double[] fs, double[] fis)
+        {
+            Points = new List<DataPoint>();
+            for (int n = 0; n < N; n++)
+            {
+                int length = fs.Length;
+                double F = 0;
+                for (int i = 0; i < length; i++)
+                {
+                    F += A * Math.Sin(2 * Math.PI * fs[i] * n / N + fis[i]);
+                }
+                Points.Add(new DataPoint(n, F));
+            }
+        }
+
+        public Signal(int N, double A, double f, double fi, double rate, bool asc)
+        {
+            double LinearModificate(double parametr, int n)
+            {
+                int sign = asc ? 1 : -1;
+                return parametr + sign * rate * parametr * n / N;
+            }
+
+            Points = new List<DataPoint>();
+            for (int n = 0; n < N; n++)
+            {
+                Points.Add(new DataPoint(n, LinearModificate(A, n) * Math.Sin(2 * Math.PI * LinearModificate(f, n) * n / N + LinearModificate(fi, n))));
+            } 
+        }
+    }
+}
